cfa5c92bc6b51b0de443a6dd9815ffdd616c26f5
Md. Arifur Rahman <uap.arifur@gmail.com>
2024-10-05 00:06:31 +0600

    first commit

--- lib/src/features/home_screen/ui/screens/home_screen.dart
+++ lib/src/features/home_screen/ui/screens/home_screen.dart
@@ -24,127 +24,148 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: GetBuilder<HomeScreenController>(initState: (state) {
           homeScreenController.getMemesApiCall().then(
-            (value) {
+                (value) {
               homeScreenController.filteredMemes =
-                  homeScreenController.memesResponseModel.value.data!.memes!;
+              homeScreenController.memesResponseModel.value.data!.memes!;
             },
           );
         }, builder: (logic) {
-          return Column(
-            children: [
-              TextFormField(
-                controller: homeScreenController.controller.value,
-                onChanged: (value) {
-                  final filtered =
-                      homeScreenController.filteredMemes.where((fruit) {
-                    return fruit.name!.contains(value) ||
-                        fruit.name!.contains(value);
-                  }).toList();
-                  homeScreenController.filteredMemes = filtered;
-                  homeScreenController.update(["searchUpdate"]);
-                },
-                decoration: InputDecoration(labelText: 'Search Fruits'),
-              ),
-              Expanded(
-                child: Obx(() {
-                  return homeScreenController.isLoginLoading.value
+          return Obx(() {
+            return Column(
+              children: [
+                TextField(
+                  controller: homeScreenController.controller.value,
+                  onChanged: (value) {
+                    final filtered =
+                    homeScreenController.filteredMemes.where((filterText) {
+                      return filterText.name!.contains(value) ||
+                          filterText.name!.contains(value);
+                    }).toList();
+                    homeScreenController.filteredMemes = filtered;
+                    homeScreenController.update(["searchUpdate"]);
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                      const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide:
+                      const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                      const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    hintText: 'Search by name',
+                    hintStyle: const TextStyle(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Expanded(
+                  child: homeScreenController.isLoginLoading.value
                       ? const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.pink,
+                    child: CircularProgressIndicator(
+                      color: Colors.pink,
+                    ),
+                  )
+                      : GetBuilder<HomeScreenController>(
+                      id: "searchUpdate",
+                      builder: (logic) {
+                        return homeScreenController.filteredMemes.length == 0
+                            ? Container(
+                          child: const Center(
+                            child: Text("Nothing Found"),
                           ),
                         )
-                      : GetBuilder<HomeScreenController>(
-                          id: "searchUpdate",
-                          builder: (logic) {
-                            return homeScreenController.filteredMemes.length ==
-                                    0
-                                ? Container(child: Text("Nothing Found"))
-                                : Container(
-                                    color: const Color(0xff2b2b34),
-                                    child: ListView.builder(
-                                      itemBuilder: (context, index) {
-                                        return GestureDetector(
-                                          onTap: () {
-                                            RouteGenerator.pushNamed(context,
-                                                Routes.memeDetailsScreen,
-                                                arguments: [
-                                                  homeScreenController
-                                                      .filteredMemes[index]
-                                                ]);
-                                          },
-                                          child: Container(
-                                            decoration: const BoxDecoration(
-                                                color: Color(0xff444155),
-                                                borderRadius: BorderRadius.only(
-                                                    topLeft:
-                                                        Radius.circular(25),
-                                                    topRight:
-                                                        Radius.circular(25),
-                                                    bottomLeft:
-                                                        Radius.circular(10),
-                                                    bottomRight:
-                                                        Radius.circular(10))),
-                                            margin: const EdgeInsets.only(
-                                              left: 15,
-                                              right: 15,
-                                              bottom: 10,
+                            : Container(
+                          color: const Color(0xff2b2b34),
+                          child: ListView.builder(
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  RouteGenerator.pushNamed(
+                                      context, Routes.memeDetailsScreen,
+                                      arguments: [
+                                        homeScreenController
+                                            .filteredMemes[index]
+                                      ]);
+                                },
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                      color: Color(0xff444155),
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(25),
+                                          topRight: Radius.circular(25),
+                                          bottomLeft:
+                                          Radius.circular(10),
+                                          bottomRight:
+                                          Radius.circular(10))),
+                                  margin: const EdgeInsets.only(
+                                    left: 15,
+                                    right: 15,
+                                    bottom: 10,
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                    children: [
+                                      Card(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(15),
+                                          //set border radius more than 50% of height and width to make circle
+                                        ),
+                                        child: Column(
+                                          children: [
+                                            ClipRRect(
+                                              borderRadius:
+                                              const BorderRadius
+                                                  .all(
+                                                Radius.circular(15),
+                                              ),
+                                              child: Image.network(
+                                                homeScreenController
+                                                    .filteredMemes[
+                                                index]
+                                                    .url!,
+                                              ),
                                             ),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Card(
-                                                  shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            15),
-                                                    //set border radius more than 50% of height and width to make circle
-                                                  ),
-                                                  child: Column(
-                                                    children: [
-                                                      ClipRRect(
-                                                        borderRadius:
-                                                            const BorderRadius
-                                                                .all(
-                                                          Radius.circular(15),
-                                                        ),
-                                                        child: Image.network(
-                                                          homeScreenController
-                                                              .filteredMemes[
-                                                                  index]
-                                                              .url!,
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                ),
-                                                Container(
-                                                  padding:
-                                                      const EdgeInsets.all(10),
-                                                  child: Text(
-                                                    homeScreenController
-                                                        .filteredMemes[index]
-                                                        .name!,
-                                                    style: const TextStyle(
-                                                      color: Colors.white,
-                                                      fontWeight:
-                                                          FontWeight.bold,
-                                                      fontSize: 17,
-                                                    ),
-                                                  ),
-                                                )
-                                              ],
-                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        padding:
+                                        const EdgeInsets.all(10),
+                                        child: Text(
+                                          homeScreenController
+                                              .filteredMemes[index]
+                                              .name!,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17,
                                           ),
-                                        );
-                                      },
-                                    ),
-                                  );
-                          });
-                }),
-              ),
-            ],
-          );
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            );
+          });
         }),
       ),
     );
